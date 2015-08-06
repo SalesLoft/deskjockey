11e3256b53132eca706222e001aab342b002543d
Justin Barber <barber.justin@gmail.com>
2015-08-06 15:21:31 -0400

    have an api user and an email for support tickets

--- config/initializers/zendesk.rb
+++ config/initializers/zendesk.rb
@@ -1,6 +1,6 @@
 $zendesk = ZendeskAPI::Client.new do |config|
   config.url = ENV["ZENDESK_URL"]
-  config.username = ENV["ZENDESK_EMAIL"]
+  config.username = ENV["ZENDESK_USER"]
   config.token = ENV["ZENDESK_TOKEN"]
   config.logger = Rails.logger
   config.cache = Rails.cache
